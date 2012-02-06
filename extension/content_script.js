4dbaf2a9a33d1e0b6143154525eda163d817e7cc
Thomas Steiner <steiner.thomas@gmail.com>
2012-02-06 10:55:27 +0100

    Adhere to Google JavaScript code style guide. Added react on title change. Added react on DOM node modification. Added twinkling smiley support. Extended test page.

--- extension/content_script.js
+++ extension/content_script.js
@@ -8,7 +8,7 @@
       replacement: '\u2026'
     },
     
-    // avoid orphan "a"s
+    // avoid orphan 'a's
     nonBreakingSpace: {
       regexp: new RegExp('(\\ba)\\s(\\w+\\b)', 'g'),
       replacement: '$1\u00A0$2'
@@ -16,28 +16,30 @@
     
     // don't break :-) :-( :-D :-| :-P :-p etc.
     emoticon: {
-      regexp: new RegExp('(\\:)(\\-)([\\)|\\(|\\|D||p|P])', 'g'),
+      regexp: new RegExp('([\\:|;])(\\-)([\\)|\\(|\\|D||p|P])', 'g'),
       replacement: '$1\uFEFF$2\uFEFF$3'
     },
     
-    // replace "[adjective]-ass [noun]" to "[adjective] ass-[noun]" where applicable
+    // turn "[adjective]-ass [noun]" into "[adjective] ass-[noun]"
     adjectiveAssNoun: {
       regexp: /(\w+)-(ass)(\s+)(\w+)/gi,
       replacement: function (original, adjective, ass, space, noun) {
         // is the first word really an adjective and the second really a noun?
         var taggedWords = tagger.tag([adjective.toLowerCase(), noun]);
-        if (taggedWords[0][1].match(/^JJ$/) && taggedWords[1][1].match(/^NN|^VBG$/))
-          // then perform the replacement from "smart-ass car" to "smart ass-car"
+        if ((taggedWords[0][1].match(/^JJ$/)) &&
+            (taggedWords[1][1].match(/^NN|^VBG$/))) {
+          // then replace from "smart-ass car" to "smart ass-car"
           return adjective + space + ass + '-' + noun;
-        else
+        } else {
           // otherwise, don't change anything
           return original;
+        }
       }
     }
   };
 	
   // gets all text nodes from the DOM tree starting from a given root
-  function getAllTextNodes(root) {
+  var getAllTextNodes = function getAllTextNodes(root) {
     return document.evaluate(
         './/text()[normalize-space(.) != ""]',
         root,
@@ -49,30 +51,45 @@
   var tagger = new POSTagger();
   
   // applies all rules to the given text or XPathResult
-  function applyRules(item) {
+  var applyRules = function applyRules(item) {
     // apply rules to each item of the XPathResult
     if (item instanceof XPathResult) {
       for (var i = 0, l = item.snapshotLength; i < l; i++) {
         var textNode = item.snapshotItem(i);
         textNode.data = applyRules(textNode.data);
-      }
-      return item;
+      }      
+    } else {    
+      // apply all rules to item
+      return Object.keys(rules).reduce(function(item, ruleName) {
+        var rule = rules[ruleName];
+        return item.replace(rule.regexp, rule.replacement);
+      }, item);
     }
-    
-    // apply all rules to item
-    return Object.keys(rules).reduce(function (item, ruleName) {
-      var rule = rules[ruleName];
-      return item.replace(rule.regexp, rule.replacement);
-    }, item);
   };
 	
   // initial processing
   applyRules(getAllTextNodes(document.body));
   document.title = applyRules(document.title);
   
-	// event listener to react on dynamic node insertions
+	// event listener to react on dynamic DOM node insertions
 	document.body.addEventListener('DOMNodeInserted', function(e) {
     applyRules(getAllTextNodes(e.target));
 	}, false);
+
+  // event listener to react on dynamic DOM node modifications
+	document.body.addEventListener('DOMCharacterDataModified', function(e) {
+    e.target.data = applyRules(e.target.data);
+	}, false);	
+	
+	// event listener to react on dynmaic title changes
+  var currentTitle = document.title;
+  // need the element node, not document.title for the event listener to work
+  var title = document.getElementsByTagName('title')[0];  
+  title.addEventListener('DOMSubtreeModified', function(e) {
+    if (document.title !== currentTitle) {
+      currentTitle = applyRules(document.title);        
+      document.title = currentTitle;
+    }
+  }, false);	
 	
 })();

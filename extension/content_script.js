10508b4df1e6d7ccfc143174b0f813d08ced9e07
Thomas Steiner <steiner.thomas@gmail.com>
2013-06-27 15:26:04 +0200

    Added support for manifest_version 2.

--- extension/content_script.js
+++ extension/content_script.js
@@ -11,9 +11,9 @@
         textNodes.push(walker.currentNode);
       }
     }
-    return textNodes;    
+    return textNodes;
   };
-  
+
   // applies all rules to the given text
   var applyRules = function(rules, item) {
     // apply rules to each item
@@ -22,15 +22,15 @@
         var textNode = item[i];
         // call yourself recursively
         textNode.textContent = applyRules(rules, textNode.textContent);
-      }      
-    } else {    
+      }
+    } else {
       // apply all rules to item
       return rules.reduce(function(item, rule) {
         return item.replace(rule.regexp, rule.replacement);
       }, item);
     }
   };
-  
+
   // apply initial processing and set up hooks
   var init = function(rules) {
     // initial processing
@@ -59,7 +59,7 @@
         document.title = currentTitle = applyRules(rules, document.title);
     }, false);
   };
-  
+
   // retrieve the rules, parse them, and init
   chrome.extension.sendRequest("rules", function(rules) {
     // parse rules
@@ -78,9 +78,66 @@
           }
         } catch(e) {
           alert(chrome.i18n.getMessage('invalidRegExp'));
-        }    
+        }
       }
     });
     init(rules);
   });
+
+  // Amazon
+  var anchors = document.getElementsByTagName('a');
+  var amazonAnchors = [];
+  var amazonRegExp = /^https?\:\/\/www\.amazon\.(com|ca|co\.uk|de|fr|jp)\/.*/i;
+  var associateRegExp1 = /.*?tag=(\w+\-?\w*?).*/i;
+  var associateRegExp2 = /.*?\/ASIN\/\w+\/(\w+)\/?(ref=nosim\/?)?$/i;
+  var associateRegExp3 = /.*?\/ref=nosim\/(\w+)$/i;
+  var asinRegExp1 = /.*?\/ASIN\/(\w+)\/.*/i;
+  var asinRegExp2 = /.*?\/gp\/product\/(\w+)[\/|\?]?.*/i;
+  var asinRegExp3 = /.*?\/dp\/(product-description\/)?(\w+)(\/|\?)?.*/i;
+  var searchRegExp1 = /\/gp\/search/;
+  var searchRegExp2= /\/s\?/;
+  var searchRegExp3 = /\/search\?/;
+  var host = document.location.host;
+  if ((host.indexOf('bing') === -1) &&
+      (host.indexOf('yahoo') === -1) &&
+      (host.indexOf('ask') === -1) &&
+      (host.indexOf('google') === -1)) {
+    for (var i = 0, anchor; anchor = anchors[i]; i++) {
+      var href = anchor.href;
+      if (amazonRegExp.test(href)) {
+        var marketPlace = href.replace(amazonRegExp, '$1');
+        var associate = '';
+        if (associateRegExp1.test(href)) {
+          associate = href.replace(associateRegExp1, '$1');
+        } else if (associateRegExp2.test(href)) {
+          associate = href.replace(associateRegExp2, '$1');
+        } else if (associateRegExp3.test(href)) {
+          associate = href.replace(associateRegExp3, '$1');
+        }
+        var asin = '';
+        if (asinRegExp1.test(href)) {
+          asin = href.replace(asinRegExp1, '$1');
+        } else if (asinRegExp2.test(href)) {
+          asin = href.replace(asinRegExp2, '$1');
+        } else if (asinRegExp3.test(href)) {
+          asin = href.replace(asinRegExp3, '$2');
+        }
+        if (asin && !associate /* don't be evil */) {
+          anchor.href = (new AmazonAssociate(marketPlace, 'blogccasion')).getSingleDetailPageUrl(asin);
+        } else if (!asin && !associate  /* don't be evil */) {
+          if ((searchRegExp1.test(href)) ||
+              (searchRegExp2.test(href)) ||
+              (searchRegExp3.test(href))) {
+            anchor.href += '&tag=' + (new AmazonAssociate(marketPlace, 'blogccasion')).getTag();
+          } else if (!associate /* don't be evil */) {
+            if (anchor.href.indexOf('?') === -1) {
+              anchor.href += '?&tag=' + (new AmazonAssociate(marketPlace, 'blogccasion')).getTag();
+            } else {
+              anchor.href += '&tag=' + (new AmazonAssociate(marketPlace, 'blogccasion')).getTag();
+            }
+          }
+        }
+      }
+    }
+  }
 })();
---
+++ extension/options_content.js
@@ -0,0 +1,8 @@
+document.getElementById('add').innerHTML =
+    chrome.i18n.getMessage('addNewRule');
+document.getElementById('reset').innerHTML =
+    chrome.i18n.getMessage('resetDefaultSettings');
+document.getElementById('instructions').innerHTML =
+    chrome.i18n.getMessage('instructions');
+document.getElementById('madeBy').innerHTML =
+    chrome.i18n.getMessage('madeInEurope');
